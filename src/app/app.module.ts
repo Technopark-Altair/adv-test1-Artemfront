8a5fcd7e4385d0537abe8b1ed0558c1dac4ae451
Artem <arpavlovscky@yandex.ru>
2021-02-04 00:26:50 +0300

    Done 1 work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './car/car.component';
+import { CarsListComponent } from './cars-list/cars-list.component';
+
+import { CarListComponent } from './components/car-list/car-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+CarsListComponent,
+
+CarListComponent,
+],
+imports: [
+BrowserModule,
+AppRoutingModule,
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
